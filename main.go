9876f2025dc0b4272ea6929df2126c1fb4287123
Christian Kelly <ckelly@afilias.info>
2018-11-14 11:22:14 +0000

    Break out DNS lookup to separate fn. Fix concurrency.

--- main.go
+++ main.go
@@ -2,12 +2,43 @@
 package main
 
 import (
+    "os"
     "fmt"
-    "net"
+    "bufio"
 )
 
 // foo.dbl.spamhaus.org
 
 func main () {
+
+    // We'll send a series of domains into the domains channel for lookup
+    domains := make(chan string, 300000)
+    // Results channel to indicate when worker is done
+    results := make(chan bool, 300000)
+
+    // Start up some workers
+    for w := 1; w <= 50; w++ {
+        go RblWorker(w, domains, results)
+    }
+
+    // Take domains from stdin and push them into the domains channel
+    scanner := bufio.NewScanner(os.Stdin)
+    items := 0
+    for scanner.Scan() {
+        domains <- scanner.Text()
+        items = items + 1
+    }
+    close(domains)
+    fmt.Printf("Pushed %d domains\n", items)
+
+    // Read back the results
+    fmt.Println("Collecting results")
+    bad_domains := 0
+    for i := 0; i < items; i++ {
+        if ! <-results {
+            bad_domains = bad_domains + 1
+        }
+    }
+    fmt.Printf("%d%% (%d/%d)\n", (100*bad_domains/items), bad_domains, items)
 
 }
--- worker.go
+++ worker.go
@@ -6,12 +6,31 @@ import (
     "net"
 )
 
-func RblWorker(worker_id int, domains <-chan string, results chan<- string) {
-    fmt.Println("Worker #%d started", worker_id)
-    for domain := range domains {
-        fmt.Println("Worker #%d testing %s", worker_id, domain)
-        net.LookupHost("example.com.dbl.spamhaus.org")
+func RblCheckOk(domain string) (bool, error) {
+    // Check if domain is in RBL
+    lookup := fmt.Sprintf("%s.dbl.spamhaus.org", domain)
+    result, err := net.LookupHost(lookup)
+    //fmt.Printf(">> %s %s\n", lookup, result)
+    if err != nil {
+        //fmt.Println("WARNING lookup for ", domain, " failed : ", err)
+        return true, err
     }
+    if len(result) < 1 {
+        return true, nil
+    }
+    return false, nil
 }
 
-
+func RblWorker(worker_id int, domains <-chan string, results chan<- bool) {
+    fmt.Printf("Worker #%d started\n", worker_id)
+    for domain := range domains {
+        //fmt.Printf("Worker #%d testing %s\n", worker_id, domain)
+        result, err := RblCheckOk(domain)
+        _ = err
+        if ! result {
+            fmt.Println(domain)
+        }
+        results <- result
+        //fmt.Printf("Worker #%d finished\n", worker_id)
+    }
+}
